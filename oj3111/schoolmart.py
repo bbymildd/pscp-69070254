21ea1c103b7e857e5f4b6682a201b3e1f4c6752e
Nutpimol <npm.mildy@gmail.com>
2026-08-13 20:13:41 +0700

    add code

--- oj3111/schoolmart.py
+++ oj3111/schoolmart.py
@@ -4,22 +4,19 @@ def main():
     """schoolmart"""
     mem = input()
     order = int(input())
-    total = 0
+    total = Decimal("0")
+
     for _ in range(order):
-        price = float(input())
+        price = Decimal(input())
         total += price
+
     if mem == "Y":
-        dis = total * 0.05
-        pay = total - dis
-        net = Decimal(str(pay)).quantize(Decimal("0.01"), rounding = ROUND_HALF_UP)
-        print(net)
-    elif mem == "N":
-        if total >= 500:
-            dis = total * 0.03
-            pay = total - dis
-            net = Decimal(str(pay)).quantize(Decimal("0.01"), rounding = ROUND_HALF_UP)
-            print(net)
-        else:
-            net = Decimal(str(total)).quantize(Decimal("0.01"), rounding = ROUND_HALF_UP)
-            print(net)
+        total *= Decimal("0.95")
+    elif total >= Decimal("500"):
+        total *= Decimal("0.97")
+
+    total = total.quantize(Decimal("0.01"),rounding=ROUND_HALF_UP)
+
+    print(total)
+
 main()
